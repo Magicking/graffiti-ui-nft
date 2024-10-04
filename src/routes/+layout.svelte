97efbee721ae40e7e28a327dd8d461ae413a5c17
s@6120.eu <s@6120.eu>
2024-10-05 01:56:59 +0200

    Fix mobile version #2 + default grey when not connected

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,23 @@
 <script>
+  import {
+    connected
+  } from "svelte-ethers-store";
   import "../app.css";
   import { chainInfo } from "$lib/stores/chainInfo";
   import Footer from "$lib/components/Footer.svelte";
 </script>
 
 <svelte:head>
+  {#if $connected}
   {#if $chainInfo.chainId === 81457}
-    <link rel="stylesheet" href="./globals.css" />
+    <link rel="stylesheet" href="./globals-81457.css" />
   {:else if $chainInfo.chainId === 8453}
-    <link rel="stylesheet" href="./globals-base.css" />
+    <link rel="stylesheet" href="./globals-8453.css" />
   {:else}
-    <link rel="stylesheet" href="./globals.css" />
+      <link rel="stylesheet" href="./globals.css" />
+    {/if}
+  {:else}
+      <link rel="stylesheet" href="./globals.css" />
   {/if}
 </svelte:head>
 <div class="min-h-screen bg-darkBackground">
